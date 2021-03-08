fc8dcdc8e5b2f3918872ba5061679fd2251b2084
Thomas Sojka <thsojka@web.de>
2021-03-08 18:47:06 +0100

    Start / Stop Stream

--- src/grootlapse/core.cljs
+++ src/grootlapse/core.cljs
@@ -1,6 +1,8 @@
 (ns grootlapse.core
-  (:require [reagent.dom :as dom]))
+  (:require [reagent.dom :as dom]
+            [reagent.core :as r]))
 (defonce server (atom nil))
+(defonce state (r/atom nil))
 
 (defn app []
   [:div
@@ -8,14 +10,22 @@
    [:main
     [:button {:on-click
               (fn []
-                (.playStream ^js @server))}
-     "Create"]
-    [:canvas {:ref (fn [ref] (prn ref)
-                     (when (and ref (= nil @server))
-                       (let [uri (str "ws://axidraw:8080")
-                             wsavc (js/window.WSAvcPlayer. ref "webgl" 1 35)]
-                         (.connect wsavc uri)
-                         (reset! server wsavc))))}]
+                (.playStream ^js @server)
+                (swap! state assoc :new-groopse true))}
+     "Start"]
+    [:button {:on-click
+              (fn []
+                (.stopStream ^js @server)
+                (swap! state assoc :new-groopse false))}
+     "stop"]
+    [:canvas#canvas
+     {:style {:display (if (:new-groopse @state) "block" "none")}
+      :ref (fn [ref] (prn ref)
+             (when (and ref (= nil @server))
+               (let [uri (str "ws://axidraw:8080")
+                     wsavc (js/window.WSAvcPlayer. ref "webgl" 1 35)]
+                 (.connect wsavc uri)
+                 (reset! server wsavc))))}]
     [:div "New GrootLapse"]]])
 
 
